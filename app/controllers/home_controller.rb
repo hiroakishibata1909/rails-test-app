7a06bf624beef9ec13c02fab176c5a7ef3031b5b
EC2 Default User <ec2-user@ip-172-31-34-56.ap-northeast-1.compute.internal>
2019-05-19 10:01:52 +0000

    after display post results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
     unless cookies[:user_id].nil?
       @login_user = User.find_by(id: cookies[:user_id])
     end
+    @posts = Post.where(user_id: @login_user.id)
   end
 end
